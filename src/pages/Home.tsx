fe45b38b3f7a430d148665d7789e2bbb86b87b82
Eris Kasa <165952691+Eris141@users.noreply.github.com>
2025-03-06 20:30:42 +0100

    feat: convert Home page to ts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,36 @@
-import { useMemo, useEffect, useState } from "react";
+import { useMemo, useEffect, useState, ReactNode } from "react";
 import ProductCart from "../components/carts/Cart";
 
+interface HomeCartProduct {
+  id: number,
+  name: string,
+  title: string,
+  quantity: number,
+  isFavorite?: boolean,
+  icon?: ReactNode,
+  price: number,
+  image: string,
+}
+
+
 const Home = () => {
-    const [ products, setProducts ] = useState([]);
+    const [ products, setProducts ] = useState<HomeCartProduct[]>([]);
+    const [ error, setError ]  = useState<string | null>(null);
 
     useEffect(() => {
       const fetchProducts = async () => {
         try {
           const response = await fetch('https://fakestoreapi.com/products');
           const data = await response.json();
-           setProducts(data.map((products) => ({...products, isFavorite: false})));
+           setProducts(data.map((products: any) => ({...products, isFavorite: false})) as HomeCartProduct[]);
         } catch (error) {
           console.error("Error fetching products", error);
+          setError("Faild to fetch products. Please try again later!")
         }
       }
       
       fetchProducts();
-    },[])
+    },[]) 
 
     const memoizedProducts = useMemo(() => products, [products]);
 
